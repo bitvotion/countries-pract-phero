d9b731b820a26a7f1a997a908efc155cabaea833
Sakif Ahmed <ahmed.sakif225@gmail.com>
2025-09-21 14:14:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+// Data Load
+
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Countries</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
